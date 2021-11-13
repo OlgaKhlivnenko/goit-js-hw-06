4f57519833e28804fa8429a42e229aa3c471b49e
OlgaKhlivnenko <xlivnenko@ukr.net>
2021-11-13 05:24:45 -0500

    HW-6js 13.11.21

--- js/task-04.js
+++ js/task-04.js
@@ -1,21 +1,19 @@
-const counterValue = 0;
-const spanEl = document.querySelector(`#value`);
 const btns = document.querySelectorAll(`button`);
  
 btns.forEach(btn => {
     btn.addEventListener(`click`, function () {
         const action = this.dataset.action;
         console.log(action);
-        const currentValue = +spanEl.textContent;
-        
+        const spanEl = document.querySelector(`#value`);
+        const currentValue = Number(spanEl.textContent);
+        let counterValue = 0;
 
-        if (action === "decrement") {
+        if (action === "increment") {
             counterValue = currentValue + 1;
-        } else {
-            counterValue = currentValue - 1 > 0 ?
-                currentValue - 1 :
-                0;
+        }
+        else {
+            counterValue = currentValue - 1 > 0 ? currentValue - 1 : 0;
         }
         spanEl.textContent = counterValue;
-    })
+    });
 })
--- js/task-08.js
+++ js/task-08.js
@@ -1,14 +1,21 @@
 const form = document.querySelector(`.login-form`);
 
 form.addEventListener(`submit`, onFormSubmit);
-
-function onFormSubmit(event) {
-    event.preventDefault();
-    const { elements: { login, password } } = event.currentTarget;
-    if (login.value === "" || password.value === "") {
-        alert(`Все поля должны быть заполнены!`)
-    }
     
-    event.currentTarget.resert();
+    function onFormSubmit(event) {
+        event.preventDefault();
+        const FormEl = event.currentTarget.elements
+        
+        const email = FormEl.email.value;
+        const password = FormEl.password.value;
+        
+    if (email === "" || password === "") {
+        alert(`Все поля должны быть заполнены!`)
+        }
+  
+        const formData = { email, password };
+        console.log(formData);
+        
+        form.reset();
 }
 
